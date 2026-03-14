56311149087b24fdd3cccac92d2ac7cc32fd267c
AnotherTimmyJapan <timmytyanpuipuisub2@gmail.com>
2026-03-14 22:15:41 +0900

    Update makecontinuous.sh

--- makecontinuous.sh
+++ makecontinuous.sh
@@ -18,4 +18,3 @@ tools/sm64tools/n64cksum build/jp/sm64.jp.z64
 make patch -j GRUCODE=f3d_20E
 cp build/jp/sm64.jp.bps "$1-$2-hle.bps"
 cp build/jp/sm64.jp.map "sm64.jp.map"
-cp build/jp/sm64.jp.elf "sm64.jp.elf"
